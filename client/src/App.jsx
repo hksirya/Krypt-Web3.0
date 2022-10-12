d19cf02fe700dd1116525a8823560450fc3cfb99
Harsh kumar Sirya <104431269+hksirya@users.noreply.github.com>
2022-10-12 20:16:40 +0530

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import {
+  Loader,
+  Footer,
+  Transactions,
+  Navbar,
+  Services,
+  Welcome,
+} from "./components";
+const App = () => {
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from "./Transactions.json";
+
+export const contractABI = abi.abi;
+export const contractAddress = "0x835209A32eAEB0c78b8BD0Dda1C2400c4A952c64";
